e0de28f892d756ad2d646d83402a0b31f1c45635
germanheinz <heinz.german@gmail.com>
2020-10-11 18:45:17 +0200

    First Commit

---
+++ src/main/java/com/germanheinz/servicesjms/ServicesJmsApplication.java
@@ -0,0 +1,13 @@
+package com.germanheinz.servicesjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/germanheinz/servicesjms/ServicesJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanheinz.servicesjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
