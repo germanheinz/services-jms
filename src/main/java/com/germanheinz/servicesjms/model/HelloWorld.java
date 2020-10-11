bb8c537433af141ab76308d57fd96ac03df8d2dd
germanheinz <heinz.german@gmail.com>
2020-10-11 18:56:25 +0200

    Embedded Server Configuration

---
+++ src/main/java/com/germanheinz/servicesjms/model/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.germanheinz.servicesjms.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorld implements Serializable {
+
+    private UUID id;
+    private String message;
+
+}
